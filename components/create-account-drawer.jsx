d6977f53762bef332fc823b057129c9da21fa72e
Sanskar-12 <sanskarv2004@gmail.com>
2025-01-02 14:27:58 +0530

    making dashboard page

--- components/create-account-drawer.jsx
+++ components/create-account-drawer.jsx
@@ -2,34 +2,26 @@
 
 import {
   Drawer,
-  DrawerClose,
   DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
 import { useState } from "react";
-import { Button } from "./ui/button";
 
 const CreateAccountDrawer = ({ children }) => {
   const [open, setOpen] = useState(false);
 
   return (
     <Drawer open={open} onOpenChange={setOpen}>
-      <DrawerTrigger>{children}</DrawerTrigger>
+      <DrawerTrigger asChild>{children}</DrawerTrigger>
       <DrawerContent>
         <DrawerHeader>
-          <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-          <DrawerDescription>This action cannot be undone.</DrawerDescription>
+          <DrawerTitle>Create New Account</DrawerTitle>
         </DrawerHeader>
-        <DrawerFooter>
-          <Button>Submit</Button>
-          <DrawerClose asChild>
-            <Button variant="outline">Cancel</Button>
-          </DrawerClose>
-        </DrawerFooter>
+        <div>
+          <form></form>
+        </div>
       </DrawerContent>
     </Drawer>
   );
