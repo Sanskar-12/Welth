1a0f4c5b071ea85319a4317330b40800cc7c2081
Sanskar-12 <sanskarv2004@gmail.com>
2025-01-06 18:17:23 +0530

    creating budget progress UI

--- components/budget-progress.jsx
+++ components/budget-progress.jsx
@@ -1,6 +1,17 @@
 "use client";
 
 import { useState } from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { Check, Pencil, X } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const BudgetProgress = ({ initialBudget, currentExpenses }) => {
   const [isEditing, setIsEditing] = useState(false);
@@ -12,7 +23,74 @@ const BudgetProgress = ({ initialBudget, currentExpenses }) => {
     ? (currentExpenses / initialBudget.amount) * 100
     : 0;
 
-  return <div>BudgetProgress</div>;
+  const budgetUpdateHandler = () => {};
+
+  const budgetCancelHandler = () => {
+    setNewBudget(initialBudget?.amount?.toString() || "");
+    setIsEditing(false);
+  };
+
+  return (
+    <Card>
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <div className="flex-1">
+          <CardTitle>Monthly Budget ( Default Account )</CardTitle>
+          <div className="flex items-center gap-2 mt-1">
+            {isEditing ? (
+              <div className="flex items-center gap-2 ">
+                <Input
+                  placeholder="Enter Amount"
+                  value={newBudget}
+                  onChange={(e) => setNewBudget(e.target.value)}
+                  type="number"
+                  className="w-32"
+                  autoFocus
+                />
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  onClick={budgetUpdateHandler}
+                >
+                  <Check className="h-4 w-4 text-green-500" />
+                </Button>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  onClick={budgetCancelHandler}
+                >
+                  <X className="h-4 w-4 text-red-500" />
+                </Button>
+              </div>
+            ) : (
+              <>
+                <CardDescription>
+                  {initialBudget
+                    ? `₹${currentExpenses.toFixed(
+                        2
+                      )} of ₹${initialBudget.amount.toFixed(2)} spent`
+                    : "No budget set"}
+                </CardDescription>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  onClick={() => setIsEditing(true)}
+                  className="h-6 w-6"
+                >
+                  <Pencil className="h-3 w-3" />
+                </Button>
+              </>
+            )}
+          </div>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default BudgetProgress;
