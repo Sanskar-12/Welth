7faef0dee9f8127596d258928eb15554e3de4670
Sanskar-12 <sanskarv2004@gmail.com>
2025-01-08 18:01:12 +0530

    creating the transaction form

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -3,13 +3,13 @@ import AddTransactionForm from "@/components/add-transaction-form";
 import { defaultCategories } from "@/data/categories";
 
 const CreateTransactionPage = async () => {
-  const accounts = await getUserAccounts();
+  const { data } = await getUserAccounts();
 
   return (
     <div className="max-w-3xl mx-auto px-5">
       <h1 className="text-5xl gradient-title mb-8">Add Transaction</h1>
 
-      <AddTransactionForm accounts={accounts} categories={defaultCategories} />
+      <AddTransactionForm accounts={data} categories={defaultCategories} />
     </div>
   );
 };
--- components/add-transaction-form.jsx
+++ components/add-transaction-form.jsx
@@ -1,5 +1,145 @@
+"use client";
+
+import { createTransaction } from "@/actions/transaction";
+import { useFetchData } from "@/hooks/use-fetch";
+import { transactionSchema } from "@/lib/zod-schema/schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Input } from "./ui/input";
+import CreateAccountDrawer from "./create-account-drawer";
+import { Button } from "./ui/button";
+
 const AddTransactionForm = ({ accounts, categories }) => {
-  return <div>AddTransactionForm</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    watch,
+    reset,
+    getValues,
+  } = useForm({
+    resolver: zodResolver(transactionSchema),
+    defaultValues: {
+      type: "EXPENSE",
+      amount: "",
+      description: "",
+      accountId: accounts?.find((account) => account.isDefault?.id),
+      date: new Date(),
+      isRecurring: false,
+    },
+  });
+
+  const {
+    data: createTransactionData,
+    loading: createTransactionDataLoading,
+    error,
+    fn: createTransactionFn,
+  } = useFetchData(createTransaction);
+
+  const filteredCategories = categories.filter(
+    (category) => category.type === watch("type")
+  );
+
+  return (
+    <form className="space-y-6">
+      {/* AI Receipt Scanner */}
+
+      <div className="space-y-2">
+        <label className="text-sm font-medium">Type</label>
+        <Select
+          onValueChange={(value) => setValue("type", value)}
+          defaultValue={watch("type")}
+        >
+          <SelectTrigger>
+            <SelectValue placeholder="Select Type" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="EXPENSE">Expense</SelectItem>
+            <SelectItem value="INCOME">Income</SelectItem>
+          </SelectContent>
+        </Select>
+
+        {errors.type && (
+          <p className="text-sm text-red-500">{errors.type.message}</p>
+        )}
+      </div>
+      <div className="grid gap-6 md:grid-cols-2">
+        <div className="space-y-2">
+          <label className="text-sm font-medium">Amount</label>
+          <Input
+            placeholder="0.00"
+            {...register("amount")}
+            step="0.01"
+            type="number"
+          />
+
+          {errors.amount && (
+            <p className="text-sm text-red-500">{errors.amount.message}</p>
+          )}
+        </div>
+        <div className="space-y-2">
+          <label className="text-sm font-medium">Account</label>
+          <Select
+            onValueChange={(value) => setValue("accountId", value)}
+            defaultValue={getValues("accountId")}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="Select Account" />
+            </SelectTrigger>
+            <SelectContent>
+              {accounts.map((account) => (
+                <SelectItem value={account.id} key={account.id}>
+                  {account.name} (₹{parseFloat(account.balance).toFixed(2)})
+                </SelectItem>
+              ))}
+              <CreateAccountDrawer>
+                <Button
+                  variant="ghost"
+                  className="w-full select-none items-center text-sm outline-none"
+                >
+                  Create Account
+                </Button>
+              </CreateAccountDrawer>
+            </SelectContent>
+          </Select>
+
+          {errors.accountId && (
+            <p className="text-sm text-red-500">{errors.accountId.message}</p>
+          )}
+        </div>
+      </div>
+      <div className="space-y-2">
+        <label className="text-sm font-medium">Category</label>
+        <Select
+          onValueChange={(value) => setValue("category", value)}
+          defaultValue={getValues("category")}
+        >
+          <SelectTrigger>
+            <SelectValue placeholder="Select Category" />
+          </SelectTrigger>
+          <SelectContent>
+            {filteredCategories.map((category) => (
+              <SelectItem value={category.id} key={category.id}>
+                {category.name}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+
+        {errors.category && (
+          <p className="text-sm text-red-500">{errors.category.message}</p>
+        )}
+      </div>
+    </form>
+  );
 };
 
 export default AddTransactionForm;
