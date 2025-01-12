5f1f20f7150c99fae392cab681d3065d62b33d07
Sanskar-12 <sanskarv2004@gmail.com>
2025-01-12 13:14:00 +0530

    UI of dashboard data

--- components/dashboard-overview.jsx
+++ components/dashboard-overview.jsx
@@ -1,5 +1,81 @@
+"use client";
+
+import { useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 const DashboardOverview = ({ accounts, transactions }) => {
-  return <div>DashboardOverview</div>;
+  const [selectedAccountId, setSelectedAccountId] = useState(
+    accounts.find((account) => account.isDefault)?.id || accounts[0]?.id
+  );
+
+  const accountTransactions = transactions.filter(
+    (transaction) => transaction.accountId === selectedAccountId
+  );
+
+  const recentTransactions = accountTransactions
+    .sort((a, b) => new Date(b.date) - new Date(a.date))
+    .slice(0, 5);
+
+  return (
+    <div className="grid gap-4 md:grid-cols-2">
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-4">
+          <CardTitle className="text-base font-normal">
+            Recent Transactions
+          </CardTitle>
+
+          <Select
+            value={selectedAccountId}
+            onValueChange={setSelectedAccountId}
+          >
+            <SelectTrigger className="w-[140px]">
+              <SelectValue placeholder="Select Account" />
+            </SelectTrigger>
+            <SelectContent>
+              {accounts.map((account) => (
+                <SelectItem value={account.id} key={account.id}>
+                  {account.name}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 };
 
 export default DashboardOverview;
