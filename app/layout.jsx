c7d9b1d23ca6368aa94c0e8a18bdfe96c3b657ec
Sanskar-12 <sanskarv2004@gmail.com>
2025-01-13 11:22:58 +0530

    App completed

--- app/layout.jsx
+++ app/layout.jsx
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
           {/* Footer */}
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
-              Made by Sanskar
+              Made by Sanskar Vishwakarma
             </div>
           </footer>
         </body>
